448c8a153a9e886922fd888953984be994e4e676
Michal Jozwa <57333359+MichalJozwa@users.noreply.github.com>
2019-11-27 00:12:04 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+// NavList
+
+const navSlide = () => {
+    const burger = document.querySelector('.icon');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click',()=> {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index)=> {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 1s ease forwards`
+            }
+        });
+        // Burger Animation
+        burger.classList.toggle('icon-active');
+    });
+}
+
+navSlide();
+
+// Burger
+
+$(document).ready(function() {
+    $('.icon').click(function() {
+        $('.icon').toggleClass('icon_active');
+    })
+})
+
+// Jezeli w mobile menu jest wysuniete, po kliknieciu na body schowaj
